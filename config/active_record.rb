be8a80f471d562d7ac1f2382a28911a63f109258
Philip Poots <philip.poots@gmail.com>
2013-01-22 23:15:33 +0000

    Configure ActiveRecord for Heroku

--- config/active_record.rb
+++ config/active_record.rb
@@ -1,8 +1,23 @@
 require 'active_record'
 require 'logger'
+require 'uri'
 
-dbconfig = YAML::load(File.open('config/database.yml'))
 ActiveRecord::Base.logger = Logger.new(STDOUT)
-
 ActiveRecord::Migration.verbose = true
-ActiveRecord::Base.establish_connection dbconfig[ENV['RACK_ENV']]
+
+if ENV['RACK_ENV'] = 'development'
+  dbconfig = YAML::load(File.open('config/database.yml'))
+  ActiveRecord::Base.establish_connection dbconfig[ENV['RACK_ENV']]
+else
+  db = URI.parse(ENV['DATABASE_URL'])
+
+  ActiveRecord::Base.establish_connection(
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :port     => db.port,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+end
