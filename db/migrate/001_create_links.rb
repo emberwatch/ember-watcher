9052ceb87be99f1ccaa31f452fff51f3a05aa672
Philip Poots <philip.poots@gmail.com>
2013-02-02 09:00:18 +0000

    Link#posted_at along with indices

--- db/migrate/001_create_links.rb
+++ db/migrate/001_create_links.rb
@@ -2,8 +2,13 @@ class CreateLinks < ActiveRecord::Migration
   def self.up
     create_table :links do |t|
       t.string :tweet_id, :url
-      t.datetime :created_at
+      t.datetime :posted_at
+      t.timestamps
     end
+
+    add_index :links, :posted_at, order: { posted_at: :asc, created_at: :asc }
+    add_index :links, :tweet_id
+    add_index :links, :url
   end
 
   def self.down
